fix env normalization logic

algorithms/offline/any_percent_bc_minari.py
@@ -54,13 +54,19 @@ def normalize_states(states: np.ndarray, mean: np.ndarray, std: np.ndarray):
 
 def wrap_env(
     env: gym.Env,
-    state_mean: np.ndarray,
-    state_std: np.ndarray,
+    state_mean: np.ndarray = 0.0,
+    state_std: np.ndarray = 1.0,
+    reward_scale: float = 1.0,
 ) -> gym.Env:
     def normalize_state(state):
         return (state - state_mean) / state_std
 
+    def scale_reward(reward):
+        return reward_scale * reward
+
     env = gym.wrappers.TransformObservation(env, normalize_state)
+    if reward_scale != 1.0:
+        env = gym.wrappers.TransformReward(env, scale_reward)
     return env
 
 